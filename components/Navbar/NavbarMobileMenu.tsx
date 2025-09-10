04ce650270ff98dee1d3a22b57d0a109ea97e3a7
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-10 16:03:15 +0600

    Add NavbarMobileMenu component with menu items

---
+++ components/Navbar/NavbarMobileMenu.tsx
@@ -0,0 +1,28 @@
+import { NavbarMenu, NavbarMenuItem } from "@heroui/navbar";
+import { Button } from "@heroui/button";
+
+export const NavbarMobileMenu = () => {
+  const menuItems = [
+    "Home",
+    "Chat History", 
+    "Settings",
+    "Help & Support",
+    "About"
+  ];
+
+  return (
+    <NavbarMenu className="backdrop-blur-md bg-background/90">
+      {menuItems.map((item, index) => (
+        <NavbarMenuItem key={`${item}-${index}`}>
+          <Button
+            className="w-full justify-start"
+            variant="light"
+            size="lg"
+          >
+            {item}
+          </Button>
+        </NavbarMenuItem>
+      ))}
+    </NavbarMenu>
+  );
+};
