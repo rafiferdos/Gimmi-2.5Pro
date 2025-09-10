cbd46be7de940d6ae0c1b4819a93a4cb5cfdbae2
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-10 16:05:36 +0600

    Refactor UserDropdown component imports for consistency

--- components/Navbar/UserDropdown.tsx
+++ components/Navbar/UserDropdown.tsx
@@ -1,40 +1,41 @@
-import { 
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-  Avatar 
-} from "@heroui/react";
+import {
+   Dropdown,
+   DropdownItem,
+   DropdownMenu,
+   DropdownTrigger,
+} from "@heroui/dropdown";
+
+import { Avatar } from "@heroui/avatar";
 
 export const UserDropdown = () => {
-  return (
-    <Dropdown placement="bottom-end">
-      <DropdownTrigger>
-        <Avatar
-          isBordered
-          as="button"
-          className="transition-transform hover:scale-110"
-          color="primary"
-          name="User"
-          size="sm"
-          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-        />
-      </DropdownTrigger>
-      <DropdownMenu 
-        aria-label="Profile Actions" 
-        variant="flat"
-        className="backdrop-blur-md"
-      >
-        <DropdownItem key="profile" className="h-14 gap-2">
-          <p className="font-semibold">Signed in as</p>
-          <p className="font-semibold">user@example.com</p>
-        </DropdownItem>
-        <DropdownItem key="settings">Settings</DropdownItem>
-        <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-        <DropdownItem key="logout" color="danger">
-          Log Out
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  );
+   return (
+      <Dropdown placement='bottom-end'>
+         <DropdownTrigger>
+            <Avatar
+               isBordered
+               as='button'
+               className='transition-transform hover:scale-110'
+               color='primary'
+               name='User'
+               size='sm'
+               src='https://i.pravatar.cc/150?u=a042581f4e29026704d'
+            />
+         </DropdownTrigger>
+         <DropdownMenu
+            aria-label='Profile Actions'
+            variant='flat'
+            className='backdrop-blur-md'
+         >
+            <DropdownItem key='profile' className='h-14 gap-2'>
+               <p className='font-semibold'>Signed in as</p>
+               <p className='font-semibold'>user@example.com</p>
+            </DropdownItem>
+            <DropdownItem key='settings'>Settings</DropdownItem>
+            <DropdownItem key='help_and_feedback'>Help & Feedback</DropdownItem>
+            <DropdownItem key='logout' color='danger'>
+               Log Out
+            </DropdownItem>
+         </DropdownMenu>
+      </Dropdown>
+   );
 };
