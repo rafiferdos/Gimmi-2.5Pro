f4f8849c4f95c85f2287ec32fc08450cbab98265
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-12 10:06:04 +0600

    refactor: improve formatting and indentation in Badge component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,53 @@
 import { fontSans } from "@/config/fonts";
 import { siteConfig } from "@/config/site";
+
 import "@/styles/globals.css";
 import clsx from "clsx";
 import { Metadata, Viewport } from "next";
+
 import TopNavbar from "../components/navbar/index";
+
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-   title: {
-      default: siteConfig.name,
-      template: `%s - ${siteConfig.name}`,
-   },
-   description: siteConfig.description,
-   icons: {
-      icon: "/favicon.ico",
-   },
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export const viewport: Viewport = {
-   themeColor: [
-      { media: "(prefers-color-scheme: light)", color: "white" },
-      { media: "(prefers-color-scheme: dark)", color: "black" },
-   ],
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({
-   children,
+  children,
 }: {
-   children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-   return (
-      <html suppressHydrationWarning lang='en'>
-         <head />
-         <body
-            className={clsx(
-               "min-h-screen text-foreground bg-background font-sans antialiased",
-               fontSans.variable
-            )}
-         >
-            <Providers
-               themeProps={{ attribute: "class", defaultTheme: "dark" }}
-            >
-               <div className='relative flex flex-col min-h-screen'>
-                  <TopNavbar />
-                  <main className='flex-grow'>{children}</main>
-               </div>
-            </Providers>
-         </body>
-      </html>
-   );
+  return (
+    <html suppressHydrationWarning lang='en'>
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen text-foreground bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className='relative flex flex-col min-h-screen'>
+            <TopNavbar />
+            <main className='flex-grow'>{children}</main>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
